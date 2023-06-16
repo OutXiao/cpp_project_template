effaaf5ee4db3a2cca64fa181af9ede10df4c074
Jianqiang Feng <jqfeng@birentech.com>
2023-06-16 16:49:53 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "hello test" << endl;
+    const string str = "hello ";
+    int i = 0;
+    i = 5;
+    cout << i << endl;
+    cout << str << endl;
+    return 0;
+}
